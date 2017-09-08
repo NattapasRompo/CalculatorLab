70187788f93b1ae22ef15af5585bc17b2cb6b362
NattapasRompo <Nattapas.rompo@gmail.com>
2017-09-08 16:37:36 +0700

    finsih rpn

---
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPE200Lab1
+{
+    class RPNCalculatorEngine : CalculatorEngine
+
+    {
+        public string Process(string str)
+        {
+            string[] parts = str.Split(' ');
+            Stack rpnstack = new Stack ();
+            for(int i=0;i<parts.Length;i++)
+            {
+                if(parts[i] =="+" || parts[i] == "-" || parts[i] == "X" || parts[i] == "÷")
+                {
+                    string second = Convert.ToString(rpnstack.Pop());
+                    string first = Convert.ToString(rpnstack.Pop());
+                    rpnstack.Push(calculate(parts[i],first,second));
+
+
+                }
+                else
+                {
+                    rpnstack.Push(parts[i]);
+                }
+            }
+            return Convert.ToString(rpnstack.Pop());
+
+        }
+    }
+}
